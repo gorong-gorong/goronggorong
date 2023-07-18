b2cace3761369c17b665654b61549010c23f9e2f
cham2 <dearcham2@gmail.com>
2023-07-18 16:43:17 +0900

    Fix: 홈, 상품상세페이지 axios 호출 로직 분리

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -19,67 +19,68 @@ export const getItemById = async (itemId) => {
       method: 'get',
       url: `/api/v1/products?id=${itemId}`,
     });
-    const item = res.data.info;
 
-    itemImgs.forEach((itemImg) => {
-      itemImg.setAttribute('src', item.imgUrl);
-      itemImg.setAttribute('alt', `${item.name} 대표 이미지`);
-      itemImg.onerror = () => {
-        itemImg.onerror = null;
-        itemImg.setAttribute('src', '../img/error.png');
-        itemImg.style.border = '1px solid #e8e8e8';
-      };
-    });
-
-    names.forEach((name) => (name.innerText = item.name));
-
-    prices.forEach((price) => (price.innerText = item.price.toLocaleString()));
-
-    category.innerText = item.category;
-    description.innerText = item.description;
-
-    totalPrice.innerHTML = item.price.toLocaleString();
-    navAmount.addEventListener('change', () => {
-      totalPrice.innerHTML = (navAmount.value * item.price).toLocaleString();
-    });
-
-    let cartItem = [];
-    const addCart = () => {
-      const newItem = {
-        id: item._id,
-        imgUrl: item.imgUrl,
-        name: item.name,
-        price: item.price.toLocaleString(),
-        amount: navAmount.value,
-      };
-      //스토리지에 기존 아이템이 있는 경우
-      if (localStorage.getItem('cart')) {
-        cartItem = JSON.parse(localStorage.getItem('cart'));
-        //cartItem에 이름이 같은 아이템이 있는지 찾기
-        const existingItem = cartItem.find((item) => item.name === newItem.name);
-        if (existingItem) {
-          alert('기존에 장바구니에 추가 된 아이템입니다. 수량을 변경했어요😽');
-          //새로운 수량입력값으로 변경
-          existingItem.amount = navAmount.value;
-        } else {
-          cartItem.push(newItem);
-          window.alert('장바구니에 아이템을 추가했습니다🐶');
-        }
-      }
-      //기존 스토리지에 아이템이 없는 경우
-      else {
-        cartItem.push(newItem);
-        window.alert('장바구니에 아이템을 추가했습니다🐶');
-      }
-
-      localStorage.setItem('cart', JSON.stringify(cartItem));
-      //배열 초기화
-      cartItem = [];
-    };
-    navCartBtn.addEventListener('click', addCart);
+    return res.data.info;
   } catch (err) {
     alert(err.response.data.message);
   }
 };
 
-getItemById(itemId);
+const item = await getItemById(itemId);
+
+itemImgs.forEach((itemImg) => {
+  itemImg.setAttribute('src', item.imgUrl);
+  itemImg.setAttribute('alt', `${item.name} 대표 이미지`);
+  itemImg.onerror = () => {
+    itemImg.onerror = null;
+    itemImg.setAttribute('src', '../img/error.png');
+    itemImg.style.border = '1px solid #e8e8e8';
+  };
+});
+
+names.forEach((name) => (name.innerText = item.name));
+
+prices.forEach((price) => (price.innerText = item.price.toLocaleString()));
+
+category.innerText = item.category;
+description.innerText = item.description;
+
+totalPrice.innerHTML = item.price.toLocaleString();
+navAmount.addEventListener('change', () => {
+  totalPrice.innerHTML = (navAmount.value * item.price).toLocaleString();
+});
+
+let cartItem = [];
+const addCart = () => {
+  const newItem = {
+    id: item._id,
+    imgUrl: item.imgUrl,
+    name: item.name,
+    price: item.price.toLocaleString(),
+    amount: navAmount.value,
+  };
+  //스토리지에 기존 아이템이 있는 경우
+  if (localStorage.getItem('cart')) {
+    cartItem = JSON.parse(localStorage.getItem('cart'));
+    //cartItem에 이름이 같은 아이템이 있는지 찾기
+    const existingItem = cartItem.find((item) => item.name === newItem.name);
+    if (existingItem) {
+      alert('기존에 장바구니에 추가 된 아이템입니다. 수량을 변경했어요😽');
+      //새로운 수량입력값으로 변경
+      existingItem.amount = navAmount.value;
+    } else {
+      cartItem.push(newItem);
+      window.alert('장바구니에 아이템을 추가했습니다🐶');
+    }
+  }
+  //기존 스토리지에 아이템이 없는 경우
+  else {
+    cartItem.push(newItem);
+    window.alert('장바구니에 아이템을 추가했습니다🐶');
+  }
+
+  localStorage.setItem('cart', JSON.stringify(cartItem));
+  //배열 초기화
+  cartItem = [];
+};
+navCartBtn.addEventListener('click', addCart);
