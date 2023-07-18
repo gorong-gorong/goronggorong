0c605695b7f1ddda0c08a1f3577a01127f68a656
Sujin Jeon <mackerel0106@gmail.com>
2023-07-18 18:19:38 +0900

    Docs: auth api 문서화
    우선 signin signup 문서화 자동화 코드 완성
    signin, signup api 경로 auth로 이동

--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-import { authController } from '../controllers/index.js';
+import { userController, authController } from '../controllers/index.js';
 import { verifyToken } from '../middlewares/index.js';
 
 const authRouter = Router();
 
+authRouter.post('/signup', userController.createUser);
+
+authRouter.post('/signin', userController.verifyUser);
+
+authRouter.put('/signin/find-password', userController.findPassword);
+
+authRouter.get('get-user-info', verifyToken, authController.getUserInfo);
+
 // router.post('/refreshToken', verifyToken, authController.refreshToken); // token 재발급하는 api 있어야 함
-authRouter.get('/auth/get-user-info', verifyToken, authController.getUserInfo);
 
 export default authRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,13 +8,13 @@ import viewRouter from './view-router';
 const router = Router();
 
 // API
+router.use('/api/v1/auth', authRouter);
+
 router.use('/api/v1', userRouter);
 
 router.use('/api/v1', productRouter);
 
 router.use('/api/v1', orderRouter);
-
-router.use('/api/v1', authRouter);
 
 // ViewRouter
 router.use(viewRouter);
