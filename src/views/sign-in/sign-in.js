7d61caa35c2020e7d2833bd76ac7dd84815dcbf0
cham2 <dearcham2@gmail.com>
2023-07-19 15:05:01 +0900

    Add : 로그인 시 아이디 저장하기 기능 추가

--- src/views/sign-in/sign-in.js
+++ src/views/sign-in/sign-in.js
@@ -3,6 +3,12 @@ import { postSignin } from '../lib/Fetcher.js';
 const id = document.querySelector('.form__id');
 const pw = document.querySelector('.form__pw');
 const submitBtn = document.querySelector('.form__submit');
+const rememberCheck = document.querySelector('.form__remember--input');
+const savedId = localStorage.getItem('savedId');
+if (savedId) {
+  rememberCheck.checked = true;
+  id.value = savedId;
+}
 
 const handleSubmit = async (e) => {
   e.preventDefault();
@@ -12,6 +18,14 @@ const handleSubmit = async (e) => {
   };
   const userToken = await postSignin(data);
   localStorage.setItem('userToken', userToken);
+
+  // 아이디 저장
+  if (rememberCheck.checked) {
+    localStorage.setItem('savedId', id.value.trim());
+  } else {
+    localStorage.removeItem('savedId');
+  }
+
   window.location.href = '/';
 };
 submitBtn.addEventListener('click', handleSubmit);
