b828f61b6883b229bca56a4c7a8d36eb225a9f9a
cham2 <dearcham2@gmail.com>
2023-07-22 23:07:26 +0900

    Add : home 아이템리스트 페이지네이션 추가

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -10,7 +10,7 @@ const prodList = document.querySelector('.prod__list');
 let state = {
   category: '',
   page: 1,
-  perPage: 10,
+  perPage: 12,
 };
 
 const renderItemList = async () => {
@@ -24,6 +24,35 @@ const renderItemList = async () => {
     const itemElement = createItem(item);
     prodList.appendChild(itemElement);
   });
+
+  createPagination(itemData.maxPage);
+};
+
+// 페이지네이션
+const createPagination = (itemMaxPage) => {
+  const pagination = document.querySelector('.pagination__count');
+  pagination.innerHTML = '';
+  const maxPage = Math.ceil(itemMaxPage / state.perPage);
+  for (let i = 1; i <= maxPage; i++) {
+    const pageCount = document.createElement('button');
+    pageCount.innerText = `${i}`;
+    console.log(state.page);
+    if (i === state.page) {
+      pageCount.classList.add('pagination__button-active');
+    }
+    pagination.appendChild(pageCount);
+
+    const paginationButtons = pagination.querySelectorAll('button');
+    paginationButtons.forEach((pageButton) => {
+      pageButton.addEventListener('click', async (e) => {
+        state.page = Number(e.target.innerText);
+        const activeButton = document.querySelector('.pagination__button-active');
+        activeButton.classList.remove('pagination__button-active');
+        e.target.classList.add('pagination__button-active');
+        await renderItemList();
+      });
+    });
+  }
 };
 
 // 처음 페이지 연결 시 전체 아이템 리스트 렌더링
@@ -45,7 +74,10 @@ const handleCategoryClick = async (e) => {
     state.category = selectedCategory;
   }
 
-  renderItemList();
+  if (state.page !== 1) {
+    state.page = 1; // 페이지 초기화
+    await renderItemList();
+  }
 };
 
 // 각 카테고리에 이벤트 등록
