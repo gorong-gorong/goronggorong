ae71599e28006918a25b2cb8d46be0130740cb44
Sujin Jeon <mackerel0106@gmail.com>
2023-07-24 18:02:40 +0900

    Refactor: products 파트 수정
    익명함수 다 이름있는 함수로 수정
    상태코드 ENUM화
    필요없는 코드들 정리

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -1,21 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
-import { productModel } from '../db';
-import { customError } from '../middlewares';
 import { productsService } from '../services';
 
 const productsController = {
   // /products
-  getProducts: async (req, res, next) => {
+  getProducts: async function (req, res, next) {
     try {
-      // /api/v1?page=number&perPage=number
-      // /api/v1?category=string&page=number&perPage=number
       const { category, page, perPage } = req.query;
       const productList = await productsService.pagination(category, page, perPage);
-      const productAmount = productList.length;
-      const totalProductsAmount = await productsService.calculateMaximumPage(category);
+      const productListAmount = productList.length;
+      const totalProductsAmount = await productsService.calculateTotalProductsAmount(category);
 
       res.status(StatusCodes.OK).json({
-        message: `${page}페이지의 ${productAmount}개의 상품을 불러왔습니다.`,
+        message: `${page}페이지의 ${productListAmount}개의 상품을 불러왔습니다.`,
         data: {
           productList,
           totalProductsAmount,
@@ -26,72 +22,15 @@ const productsController = {
     }
   },
 
-  // id 에 해당하는 제품 불러오기
-  getProductById: async (req, res, next) => {
+  // /products/:id
+  getProductById: async function (req, res, next) {
     try {
-      // 아이디로 찾아서 JSON 으로 프론트에 쏴주기
       const { id } = req.params;
-      const product = await productsService.checkId(id);
-      res.status(200).json({
+      const product = await productsService.searchProduct(id);
+
+      res.status(StatusCodes.OK).json({
         message: '해당 아이디 제품을 불러왔습니다',
-        data: product,
-      });
-    } catch (err) {
-      next(err);
-    }
-  },
-
-  // 새로운 상품 등록하기
-  addProduct: async (req, res, next) => {
-    try {
-      // 객체 destructuring
-      const { id, category, name, price, description, amount, imgUrl } = req.body;
-      const newProduct = await productsService.addNewProduct(id, category, name, price, description, amount, imgUrl);
-      // 이미지 추가 기능을 넣는다면 addProduct({...req.body, imgUrl: 'gcp url'})
-      // 이런식으로 되어야 해서 imgUrl이 productInfo 가장 끝에 와야 할 듯
-      res.status(200).json({
-        message: '새로운 제품 등록을 완료했습니다',
-        data: newProduct,
-      });
-    } catch (err) {
-      next(err);
-    }
-  },
-
-  //기존 상품 수정
-  setProduct: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const { category, name, price, description, amount, imgUrl } = req.body;
-
-      const updatedProduct = await productsService.setExistProduct(
-        id,
-        category,
-        name,
-        price,
-        description,
-        amount,
-        imgUrl,
-      );
-      res.status(200).json({
-        message: '해당 제품 수정을 완료했습니다',
-        data: updatedProduct,
-      });
-    } catch (err) {
-      next(err);
-    }
-  },
-
-  //기존 상품 삭제
-  deleteProduct: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-
-      const deletedProduct = await productsService.deleteExistProduct(id);
-
-      res.status(200).json({
-        message: '해당 제품 삭제를 완료했습니다',
-        data: deletedProduct,
+        data: { product },
       });
     } catch (err) {
       next(err);
--- src/services/products-service.js
+++ src/services/products-service.js
@@ -4,7 +4,7 @@ import { customError } from '../middlewares';
 
 const productsService = {
   // 페이지네이션(모든 상품, 카테고리별)
-  pagination: async (category, page, perPage) => {
+  pagination: async function (category, page, perPage) {
     [page, perPage] = [Number(page), Number(perPage)];
     let productList;
 
@@ -22,96 +22,23 @@ const productsService = {
   },
 
   // 총 상품 수량 계산
-  calculateMaximumPage: async function (category) {
-    let maxPage = 0;
+  calculateTotalProductsAmount: async function (category) {
+    let totalProductsAmount = 0;
 
     if (category) {
-      maxPage = productModel.countCategoryProducts(category);
+      totalProductsAmount = productModel.countCategoryProducts(category);
     } else {
-      maxPage = productModel.countProducts();
+      totalProductsAmount = productModel.countProducts();
     }
 
-    return maxPage;
+    return totalProductsAmount;
   },
 
-  checkId: async (id) => {
+  // 상품 검색
+  searchProduct: async function (id) {
     const product = await productModel.findById(id);
-    if (!product) {
-      throw new customError(400, '해당 상품이 존재하지 않습니다');
-    }
+
     return product;
-  },
-
-  addNewProduct: async (id, category, name, price, description, amount, imgUrl) => {
-    const requiredKeys = ['id', 'category', 'name', 'price', 'description', 'amount', 'imgUrl'];
-    // req.body 엔 필수 속성이 다 있어야 함
-    if (
-      !requiredKeys.every((key) =>
-        Object.keys({ id, category, name, price, description, amount, imgUrl }).includes(key),
-      )
-    ) {
-      // 모든 필수 키 값이 존재하지 않으면
-      throw new customError(400, '등록할 내용을 모두 작성해주세요');
-    }
-    // 이외 json 데이터는 mongodb에 저장
-    const productSameId = await productModel.findById(id);
-    const productSameName = await productModel.findByName(name);
-    if (productSameId) {
-      throw new customError(400, '이미 존재하는 id 입니다');
-    }
-    if (!['Food', 'Snack', 'Toy', 'Toilet', 'Fashion'].includes(category)) {
-      throw new customError(400, '잘못된 카테고리 입니다');
-    }
-    if (productSameName) {
-      throw new customError(400, '이미 존재하는 name 입니다');
-    }
-    const newProduct = await productModel.create({
-      id,
-      category,
-      name,
-      price,
-      description,
-      amount,
-      imgUrl,
-    });
-    return newProduct;
-  },
-
-  setExistProduct: async (id, category, name, price, description, amount, imgUrl) => {
-    // 앞에 카테고리 대문자
-    const requiredKeys = ['category', 'name', 'price', 'description', 'amount', 'imgUrl'];
-    // req.body 엔 id 빼고 다 있어야 함
-    if (
-      !requiredKeys.every((key) => Object.keys({ category, name, price, description, amount, imgUrl }).includes(key))
-    ) {
-      // 모든 필수 키 값이 존재하지 않으면
-      throw new customError(400, '수정할 내용을 모두 작성해주세요');
-    }
-
-    const product = await productModel.findById(id);
-    const productSameName = await productModel.findByName(name);
-
-    if (!product) {
-      throw new customError(400, '수정할 상품이 존재하지 않습니다');
-    }
-    if (!['Food', 'Snack', 'Toy', 'Toilet', 'Fashion'].includes(category)) {
-      throw new customError(400, '잘못된 카테고리 입니다');
-    }
-    if (productSameName) {
-      throw new customError(400, '이미 존재하는 name 입니다');
-    }
-
-    const updatedProduct = await productModel.update(id, { category, name, price, description, amount, imgUrl });
-    return updatedProduct;
-  },
-
-  deleteExistProduct: async (id) => {
-    const productToDelete = await productModel.findById(id);
-    if (!productToDelete) {
-      throw new customError(400, '수정할 상품이 존재하지 않습니다');
-    }
-    const deletedProduct = await productModel.delete(id);
-    return deletedProduct;
   },
 };
 
