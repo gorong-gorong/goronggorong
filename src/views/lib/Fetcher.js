1397fd8317152ae93592054a7770e417d43df7b2
cham2 <dearcham2@gmail.com>
2023-07-25 11:44:49 +0900

    Fix : swagger바탕으로 api 요청 url 수정

--- src/views/lib/Fetcher.js
+++ src/views/lib/Fetcher.js
@@ -3,6 +3,8 @@ import * as Api from '/lib/Api.js';
 const version = 'v1';
 const domain = `/api/${version}`;
 
+// PRODUCTS
+
 // 모든 상품 조회
 export async function getItemData(category, page, perPage) {
   return await Api.Get(domain, `products?category=${category}&page=${page}&perPage=${perPage}`);
@@ -13,34 +15,33 @@ export async function getItemById(itemId) {
   return await Api.Get(domain, `products/${itemId}`);
 }
 
-// 비밀번호 확인 요청
-export async function postValidUser(data) {
-  return await Api.Post(domain, `mypage/check-valid-user`, data);
-}
+// USERS
 
 // 유저 정보 조회
 export async function getUserInfo() {
-  return await Api.Get(domain, `auth/user-info`);
+  return await Api.Get(domain, `users`);
 }
 
 // 유저 정보 수정
 export async function putUserInfo(data) {
-  return await Api.Put(domain, `/mypage/edit-user-info`, data);
-}
-
-// 유저 로그인
-export async function postSignin(data) {
-  return await Api.Post(domain, `auth/signin`, data);
+  return await Api.Put(domain, `users`, data);
 }
 
 // 회원가입
 export async function postSignup(data) {
-  return await Api.Post(domain, `auth/signup`, data);
+  return await Api.Post(domain, `users`, data);
 }
 
 // 회원탈퇴
 export async function deleteUserInfo() {
-  return await Api.Delete(domain, `mypage/delete-user-info`);
+  return await Api.Delete(domain, `users`);
+}
+
+//AUTH
+
+// 유저 로그인
+export async function postSignin(data) {
+  return await Api.Post(domain, `auth/signin`, data);
 }
 
 // 비밀번호 초기화
@@ -48,7 +49,14 @@ export async function passwordReset(data) {
   return await Api.Put(domain, `auth/signin/password-reset`, data);
 }
 
-// 주문내역 조회
+// 비밀번호 확인 요청
+export async function postValidUser(data) {
+  return await Api.Post(domain, `auth/validation`, data);
+}
+
+// ORDERS
+
+// 사용자별 주문내역 조회
 export async function getOrderList() {
   return await Api.Get(domain, `orders/user/order-list`);
 }
@@ -61,4 +69,9 @@ export async function getOrderInfo(orderId) {
 // 주문 취소
 export async function cancelOrder(orderId) {
   return await Api.Put(domain, `orders/cancel/${orderId}`);
+}
+
+// 결제
+export async function postPayment() {
+  return await Api.post(domain, `orders/payment`);
 }
