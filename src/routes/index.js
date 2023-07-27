6d33ceb0435e5f1bb258744a0bcb3dd0a21fa094
Sujin Jeon <mackerel0106@gmail.com>
2023-07-27 14:19:35 +0900

    Fix: 수정된 함수 경로 수정
    verifyToken 경로들 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import path from 'path';
 import usersRouter from './users-router';
 import authRouter from './auth-router';
 import productsRouter from './products-router';
@@ -21,5 +22,9 @@ router.use('/api/v1/orders', ordersRouter);
 
 // ViewRouter
 router.use(viewsRouter);
+
+router.use(function (req, res, next) {
+  res.status(404).sendFile(path.join(__dirname, '../views/404/404.html'));
+});
 
 export default router;
