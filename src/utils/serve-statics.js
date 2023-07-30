7aa5e63554e2100e8d52983124252ad83ac9df31
Sujin Jeon <mackerel0106@gmail.com>
2023-07-30 13:55:31 +0900

    Add: 배포 환경변수 추가

--- src/utils/serve-statics.js
+++ src/utils/serve-statics.js
@@ -2,7 +2,12 @@ import express from 'express';
 import path from 'path';
 
 const serveStatics = function (srcFolder) {
-  const src = path.join(__dirname, `../views/${srcFolder}`);
+  let src;
+  if (process.env.NODE_ENV === 'production') {
+    src = path.join(__dirname, `../../src/views/${srcFolder}`);
+  } else if (process.env.NODE_ENV === 'development') {
+    src = path.join(__dirname, `../views/${srcFolder}`);
+  }
   const option = { index: `${srcFolder}.html` };
 
   return express.static(src, option);
