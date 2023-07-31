d987ff41dbdf5105e50c678efb48c9f047568812
Ubuntu <ubuntu@ip-172-26-14-70.ap-northeast-2.compute.internal>
2023-07-31 16:02:52 +0000

    Add: mongodb 로컬 설정
    uri 수정
    db_name 추가
    product-data.json store하는 함수 구현

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,35 @@
 import mongoose from 'mongoose';
 import { createClient } from 'redis';
+const fs = require('fs');
+const path = require('path');
+const { MongoClient } = require('mongodb');
 
-// Mongoose
-mongoose.connect(process.env.MONGODB_KEY);
-
+mongoose.connect(`${process.env.MONGODB_KEY}/${process.env.MONGODB_NAME}`);
 const db = mongoose.connection;
 
 db.on('connected', () => console.log('MongoDB Connected!'));
 db.on('error', (err) => console.error('MongoDB Error', err));
+
+// Mongoose
+async function insertProductData() {
+  try {
+	// 컬렉션에 데이터가 있는지 확인
+	const productsCollection = db.collection('products');
+
+   	// product-data.json 파일 읽기
+   	const filePath = path.join(__dirname, '../../data/product-data.json');
+   	const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+   	// 데이터베이스에 데이터 삽입
+   	const result = await productsCollection.insertMany(jsonData);
+   	console.log(`MongoDB: Inserted ${result.insertedCount} records.`);
+
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
+}
+
+insertProductData();
 
 // Redis
 let options = {};
